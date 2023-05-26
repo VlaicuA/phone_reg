d68f47b78a550d2300b1ecadd26730793de531bc
AndreiV <vlaicu.andrei@yahoo.com>
2023-05-26 18:57:38 +0300

    Create Connect.php

---
+++ Connect.php
@@ -0,0 +1,25 @@
+<?php
+
+class Connect {
+
+    private $db_host = 'localhost';
+    private $db_user = 'root';
+    private $db_pass = 'root';
+    private $db_dbname = ''; //need to set it up
+    protected $db;
+
+    public function __construct(){
+        $conn = 'mysql:host='.$this->db_host.';dbname='.$this->db_dbname;
+        $this->db = new PDO($conn, $this->db_user, $this->db_pass);
+        $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        if(!$this->db){
+            echo 'not connected';
+        } else {
+            echo 'connected';
+        }
+    } 
+
+}
+
+$connect_mess = new Connect;
+
